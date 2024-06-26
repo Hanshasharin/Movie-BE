5bcdc9ad54fccfb278fe9b8c6f4c992623f602be
unknown <sharinhansha@gmail.com>
2024-06-26 08:13:28 +0530

    final

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
  
 app.use(cors({
-  origin:['http://localhost:5173','https://movie-backendserver.onrender.com'],
+  origin:['http://localhost:5173','https://movie-backendserver.onrender.com','https://movie-review-rating.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization', 'Authorization-Key'], // Specify the allowed headers
   credentials: true // Allow credentials
